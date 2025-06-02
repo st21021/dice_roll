4509bcc04f768b157e8da2bec48437aeb15f2d96
Luke Marshall <st21021@howick.school.nz>
2025-06-03 09:33:11 +1200

    Added comments to program

--- dice_roll.py
+++ dice_roll.py
@@ -7,9 +7,11 @@ def gen_rand():
     num2.set(r.randint(1,6))
     count.set(count.get()+1)
     if num1.get() == 6 and num2.get() == 6:
+        # Set label background to green
         lbl1.configure(bg="#44ff44")
         lbl2.configure(bg="#44ff44")
     else:
+        # Set label background to red
         lbl1.configure(bg="#ff4444")
         lbl2.configure(bg="#ff4444")
 
@@ -17,7 +19,7 @@ def close_window():
     '''Close the tk root window'''
     root.destroy()
 
-
+# Create window
 root = tk.Tk()
 root.title("Dice Roll")
 root.rowconfigure([0, 1, 2], minsize=30)
@@ -30,12 +32,15 @@ count = tk.IntVar(root, 0)
 btn_quit = tk.Button(root, text="Quit", command=close_window)
 btn_random = tk.Button(root, text="Random", command=gen_rand)
 
+# Labels to display dice rolls
 lbl1 = tk.Label(root, textvariable=num1, bg="#ff4444", fg="black")
 lbl2 = tk.Label(root, textvariable=num2, bg="#ff4444", fg="black")
 
+# Labels to display the number of dice rolls
 lbl_roll = tk.Label(root, text="Roll count =", justify="right")
 lbl_count = tk.Label(root, textvariable=count, justify="left")
 
+# Add everything to the window using grid
 btn_quit.grid(row=0, column=0, sticky="NSWE", padx=10, pady=3)
 btn_random.grid(row=0, column=1, sticky="NSWE", padx=10, pady=3)
 lbl1.grid(row=1, column=0, sticky="NSWE", padx=10, pady=3)
