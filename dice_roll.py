53b9ffa838e1c3d5953ee5c56a76a479eea37ebe
Luke Marshall <st21021@howick.school.nz>
2025-06-04 13:05:06 +1200

    Uses classes

--- dice_roll.py
+++ dice_roll.py
@@ -4,51 +4,57 @@ import random as r
 RED = "#ff4444"
 GREEN = "#44ff44"
 
-def gen_rand():
-    '''Generate two random numbers and display with labels'''
-    num1.set(r.randint(1,6))
-    num2.set(r.randint(1,6))
-    count.set(count.get()+1)
-    if num1.get() == 6 and num2.get() == 6:
-        # Set label background to green
-        lbl1.configure(bg=RED)
-        lbl2.configure(bg=RED)
-    else:
-        # Set label background to red
-        lbl1.configure(bg=RED)
-        lbl2.configure(bg=RED)
+class DiceRoll:
 
-def close_window():
-    '''Close the tk root window'''
-    root.destroy()
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Dice Roll")
+        self.root.rowconfigure([0, 1, 2], minsize=30)
+        self.root.columnconfigure([0, 1], minsize=150)
+
+        self.num1 = tk.IntVar(root, "")
+        self.num2 = tk.IntVar(root, "")
+        self.count = tk.IntVar(root, 0)
+
+        self.btn_quit = tk.Button(root, text="Quit", command=self.close_window)
+        self.btn_random = tk.Button(root, text="Random", command=self.gen_rand)
+
+        # Labels to display dice rolls
+        self.lbl1 = tk.Label(root, textvariable=self.num1, bg=RED, fg="black")
+        self.lbl2 = tk.Label(root, textvariable=self.num2, bg=RED, fg="black")
+
+        # Labels to display the number of dice rolls
+        self.lbl_roll = tk.Label(root, text="Roll count =", justify="right")
+        self.lbl_count = tk.Label(root, textvariable=self.count, justify="left")
+
+        # Add everything to the window using grid
+        self.btn_quit.grid(row=0, column=0, sticky="NSWE", padx=10, pady=3)
+        self.btn_random.grid(row=0, column=1, sticky="NSWE", padx=10, pady=3)
+        self.lbl1.grid(row=1, column=0, sticky="NSWE", padx=10, pady=3)
+        self.lbl2.grid(row=1, column=1, sticky="NSWE", padx=10, pady=3)
+        self.lbl_roll.grid(row=2, column=0, sticky="E", padx=10, pady=3) # Right aligned
+        self.lbl_count.grid(row=2, column=1, sticky="W", padx=10, pady=3) # Left aligned
+
+    def gen_rand(self):
+        '''Generate two random numbers and display with labels'''
+        self.num1.set(r.randint(1,6))
+        self.num2.set(r.randint(1,6))
+        self.count.set(self.count.get()+1)
+        if self.num1.get() == 6 and self.num2.get() == 6:
+            # Set label background to green
+            self.lbl1.configure(bg=RED)
+            self.lbl2.configure(bg=RED)
+        else:
+            # Set label background to red
+            self.lbl1.configure(bg=RED)
+            self.lbl2.configure(bg=RED)
+
+
+    def close_window(self):
+        '''Close the tk root window'''
+        self.root.destroy()
 
 # Create window
-root = tk.Tk()
-root.title("Dice Roll")
-root.rowconfigure([0, 1, 2], minsize=30)
-root.columnconfigure([0, 1], minsize=150)
-
-num1 = tk.IntVar(root, "")
-num2 = tk.IntVar(root, "")
-count = tk.IntVar(root, 0)
-
-btn_quit = tk.Button(root, text="Quit", command=close_window)
-btn_random = tk.Button(root, text="Random", command=gen_rand)
-
-# Labels to display dice rolls
-lbl1 = tk.Label(root, textvariable=num1, bg=RED, fg="black")
-lbl2 = tk.Label(root, textvariable=num2, bg=RED, fg="black")
-
-# Labels to display the number of dice rolls
-lbl_roll = tk.Label(root, text="Roll count =", justify="right")
-lbl_count = tk.Label(root, textvariable=count, justify="left")
-
-# Add everything to the window using grid
-btn_quit.grid(row=0, column=0, sticky="NSWE", padx=10, pady=3)
-btn_random.grid(row=0, column=1, sticky="NSWE", padx=10, pady=3)
-lbl1.grid(row=1, column=0, sticky="NSWE", padx=10, pady=3)
-lbl2.grid(row=1, column=1, sticky="NSWE", padx=10, pady=3)
-lbl_roll.grid(row=2, column=0, sticky="E", padx=10, pady=3) # Right aligned
-lbl_count.grid(row=2, column=1, sticky="W", padx=10, pady=3) # Left aligned
-
-root.mainloop()
+master = tk.Tk()
+app = DiceRoll(master)
+master.mainloop()
